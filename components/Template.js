d2d2a86d4b9f6d5639a28f4e5cf823233c0af4e5
Fadhil <boggaming098.com@gmail.com>
2024-08-05 23:05:18 +0530

    try

---
+++ components/Template.js
@@ -0,0 +1,10 @@
+// components/Template.js
+const Template = () => {
+    return (
+        <div>
+            {/* Your template content here */}
+        </div>
+    );
+};
+
+export default Template;
