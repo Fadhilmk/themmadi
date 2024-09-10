5a788060880f9583a08f5dff9c7854991c37babb
Fadhil <boggaming098.com@gmail.com>
2024-09-10 15:39:35 +0530

    bug fix

--- components/withAuth.js
+++ components/withAuth.js
@@ -4,31 +4,31 @@ import { useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebaseConfig";
 
-const withAuth = (Component) => {
-  return function AuthenticatedComponent(props) {
-    const router = useRouter();
-    const [loading, setLoading] = useState(true);
-    const [authenticated, setAuthenticated] = useState(false);
+// const withAuth = (Component) => {
+//   return function AuthenticatedComponent(props) {
+//     const router = useRouter();
+//     const [loading, setLoading] = useState(true);
+//     const [authenticated, setAuthenticated] = useState(false);
 
-    useEffect(() => {
-      const unsubscribe = onAuthStateChanged(auth, (user) => {
-        if (user) {
-          setAuthenticated(true); // User is authenticated
-        } else {
-          router.push("/login"); // Redirect to login if not authenticated
-        }
-        setLoading(false);
-      });
+//     useEffect(() => {
+//       const unsubscribe = onAuthStateChanged(auth, (user) => {
+//         if (user) {
+//           setAuthenticated(true); // User is authenticated
+//         } else {
+//           router.push("/login"); // Redirect to login if not authenticated
+//         }
+//         setLoading(false);
+//       });
 
-      return () => unsubscribe();
-    }, [router]);
+//       return () => unsubscribe();
+//     }, [router]);
 
-    if (loading) {
-      return <div>Loading...</div>; // Show loading screen while checking authentication
-    }
+//     if (loading) {
+//       return <div>Loading...</div>; // Show loading screen while checking authentication
+//     }
 
-    return authenticated ? <Component {...props} /> : null;
-  };
-};
+//     return authenticated ? <Component {...props} /> : null;
+//   };
+// };
 
-export default withAuth;
+// export default withAuth;
--- components/withNoAuth.js
+++ components/withNoAuth.js
@@ -2,22 +2,22 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import Cookies from "js-cookie";
 
-const withNoAuth = (Component) => {
-    return function NoAuthRoute(props) {
-      const router = useRouter();
+// const withNoAuth = (Component) => {
+// //     return function NoAuthRoute(props) {
+// //       const router = useRouter();
   
-      useEffect(() => {
-        const token = Cookies.get("token");
+// //       useEffect(() => {
+// //         const token = Cookies.get("token");
   
-        // If token exists, redirect to the dashboard
-        if (token) {
-          router.push(`/dashboard/${Cookies.get("userId")}`);
-        }
-      }, [router]);
+// //         // If token exists, redirect to the dashboard
+// //         if (token) {
+// //           router.push(`/dashboard/${Cookies.get("userId")}`);
+// //         }
+// //       }, [router]);
   
-      // If no token exists, render the login/signup page
-      return <Component {...props} />;
-    };
-  };
+// //       // If no token exists, render the login/signup page
+// //       return <Component {...props} />;
+// //     };
+// //   };
   
-  export default withNoAuth;
+//   export default withNoAuth;
