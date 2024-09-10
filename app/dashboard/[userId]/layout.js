598a12a761facc3b31ff917bb8285e288944a624
Fadhil <boggaming098.com@gmail.com>
2024-09-10 12:23:19 +0530

    bug fix

--- app/dashboard/[userId]/layout.js
+++ app/dashboard/[userId]/layout.js
@@ -1,15 +1,5 @@
-// import DashboardLayout from '@/components/DashboardLayout';
-
-// export default function DashboardLayoutWrapper({ children }) {
-//     return <DashboardLayout>{children}</DashboardLayout>;
-// }
-
-"use client"
 import DashboardLayout from '@/components/DashboardLayout';
-import withAuth from '@/components/withAuth';  // Import the HOC
 
-function DashboardLayoutWrapper({ children }) {
+export default function DashboardLayoutWrapper({ children }) {
     return <DashboardLayout>{children}</DashboardLayout>;
 }
-
-export default withAuth(DashboardLayoutWrapper);  // Wrap the layout with HOC
