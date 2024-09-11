2573ce40503e79fc641efa2cbe38b637cbd039c2
Ajith Poulose <ajithpls2001@gmail.com>
2024-09-11 23:26:38 +0530

    login fix

--- components/ProtectedRoute.js
+++ components/ProtectedRoute.js
@@ -1,13 +1,14 @@
 "use client"
 import { useRouter } from 'next/navigation';
 import { useEffect, useState  } from 'react';
+import Cookies from "js-cookie";
 
 const ProtectedRoute = ({ children }) => {
     const router = useRouter();
     const [isAuthenticated, setIsAuthenticated] = useState(null);
 
     useEffect(() => {
-        const token = localStorage.getItem('token');
+        const token = Cookies.get('token');
         if (!token) {
             router.push('/login');
         } else {
