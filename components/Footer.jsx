ddcde8c257710d85901a639dfb8155aa261d3bd4
Asrith Rajeevan <asrithrajeevan@Asriths-MacBook-Air.local>
2024-09-24 14:33:39 +0530

    new changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,78 +3,93 @@ import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <footer className="text-white py-2 md:py-8" style={{ backgroundColor: '#2c8ffa' }}>
-      <div className="container mx-auto px-4">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+    <footer className="text-white py-2 md:py-4" style={{ backgroundColor: '#2c8ffa' }}>
+      <div className="container mx-auto px-0">
+        <div className="grid grid-cols-1 md:grid-cols-5 gap-2">
           {/* Logo and Company Info */}
-          <div className="flex flex-col items-center md:items-start space-y-4 text-center md:text-left border-b md:border-none pb-4 md:pb-0">
-            <Image
-              src="/4.png"
-              alt="Logo"
-              width={90}
-              height={90}
-              className="object-contain"
-            />
-            <div>
-              <h2 className="text-lg font-bold text-lg">MaaDiy</h2>
-              <p className="text-sm" style={{ fontFamily: 'LeagueSpartan, sans-serif' }}>
-                Empowering your business with cutting-edge solutions.
-              </p>
+          <div className='border-b md:border-none'>
+            <div className="flex flex-col md:flex-row items-center md:justify-between space-x-4 md:space-y-0 md:space-x-4 text-center md:text-left">
+              <Image
+                src="/4.png"
+                alt="Logo"
+                width={90}
+                height={90}
+                className="object-contain"
+              />
+              <div className="md:text-left">
+                <h2 className="text-lg font-bold text-lg">MaaDiy</h2>
+                <label className="text-sm font-normal" style={{ fontFamily: 'LeagueSpartan, sans-serif' }}>
+                  Empowering your business with cutting-edge solutions.
+                </label>
+              </div>
+            </div>
+      
+            {/* Social Media Icons */}
+            <div className="flex justify-center md:justify-between p-4 pt-6">
+              <div className="flex space-x-3">
+                {/* WhatsApp */}
+                <a href="https://wa.me/7593970511" target="_blank" rel="noopener noreferrer">
+                  <Image src="/whatsapp.png" alt="WhatsApp" width={25} height={25} className="object-contain" />
+                </a>
+                {/* Instagram */}
+                <a href="https://www.instagram.com/the.maadiy?igsh=aTA2OHVzbmJ0end2" target="_blank" rel="noopener noreferrer">
+                  <Image src="/instagram24.png" alt="Instagram" width={25} height={25} className="object-contain" />
+                </a>
+                {/* Facebook */}
+                <a href="https://www.facebook.com/profile.php?id=61565378703285" target="_blank" rel="noopener noreferrer">
+                  <Image src="/facebook.png" alt="Facebook" width={25} height={25} className="object-contain" />
+                </a>
+                {/* YouTube */}
+                <a href="https://www.youtube.com/channel/UCr9BooZmN_xsauO-lLz_nLw" target="_blank" rel="noopener noreferrer">
+                  <Image src="/youtube.png" alt="YouTube" width={25} height={25} className="object-contain" />
+                </a>
+                {/* Email */}
+                <a href="mailto:therahmaneffect@gmail.com" target="_blank" rel="noopener noreferrer">
+                  <Image src="/gmail.png" alt="Email" width={25} height={25} className="object-contain" />
+                </a>
+                {/* Phone */}
+                <a href="tel:+919567565069" target="_blank" rel="noopener noreferrer">
+                  <Image src="/phone-call.png" alt="Phone" width={25} height={25} className="object-contain" />
+                </a>
+              </div>
             </div>
           </div>
 
           {/* Primary Navigation Links */}
-          <div className="flex text-lg flex-col items-center md:items-start space-y-4 text-center md:text-left border-b md:border-none pb-4 md:pb-0">
+          <div className="flex text-lg flex-col items-center space-y-3 md:items-start text-center md:text-left border-b md:border-none pb-4 md:pb-0 ml-0 md:ml-16">
             <a href="/about" className="hover:underline text-lg" style={{ fontFamily: 'LeagueSpartan, sans-serif' }}>About Us</a>
             <a href="/" className="hover:underline" style={{ fontFamily: 'LeagueSpartan, sans-serif' }}>Services</a>
             <a href="/pricing" className="hover:underline" style={{ fontFamily: 'LeagueSpartan, sans-serif' }}>Pricing</a>
-            <a href="/blog" className="hover:underline" style={{ fontFamily: 'LeagueSpartan, sans-serif' }}>Blog</a>
-            <a href="/" className="hover:underline" style={{ fontFamily: 'LeagueSpartan, sans-serif' }}>Product Overview</a>
-            <a href="/" className="hover:underline" style={{ fontFamily: 'LeagueSpartan, sans-serif' }}>Book a Demo</a>
+            <a href="/#features" className="hover:underline" style={{ fontFamily: 'LeagueSpartan, sans-serif' }}>Features</a>
           </div>
 
           {/* Secondary Navigation Links */}
-          <div className="flex text-lg flex-col items-center md:items-start space-y-4 text-center md:text-left border-b md:border-none pb-4 md:pb-0">
-            <a href="/" className="hover:underline" style={{ fontFamily: 'LeagueSpartan, sans-serif' }}>Help Center</a>
+          <div className="flex text-lg flex-col items-center md:items-start space-y-3 text-center md:text-left border-b md:border-none pb-4 md:pb-0 md:pb-0 ml-0 md:ml-16">
+            <a href="/blog" className="hover:underline" style={{ fontFamily: 'LeagueSpartan, sans-serif' }}>Blog</a>
+            <a href="/" className="hover:underline" style={{ fontFamily: 'LeagueSpartan, sans-serif' }}>Product Overview</a>
+            <a href="/" className="hover:underline" style={{ fontFamily: 'LeagueSpartan, sans-serif' }}>Book a Demo</a>
+            <a href="/#features" className="hover:underline" style={{ fontFamily: 'LeagueSpartan, sans-serif' }}>Features</a>
+          </div>
+                    
+          {/* Third Navigation Links */}
+          <div className="flex text-lg flex-col items-center md:items-start space-y-3 text-center md:text-left border-b md:border-none pb-4 md:pb-0 md:pb-0 ml-0 md:ml-16">
             <a href="/" className="hover:underline" style={{ fontFamily: 'LeagueSpartan, sans-serif' }}>Contact</a>
-            <a href="/privacy-policy" className="hover:underline" style={{ fontFamily: 'LeagueSpartan, sans-serif' }}>Privacy Policy</a>
             <a href="/terms-and-conditions" className="hover:underline" style={{ fontFamily: 'LeagueSpartan, sans-serif' }}>Terms & Conditions</a>
             <a href="/signup" className="hover:underline" style={{ fontFamily: 'LeagueSpartan, sans-serif' }}>Sign Up</a>
+            <a href="/#features" className="hover:underline" style={{ fontFamily: 'LeagueSpartan, sans-serif' }}>Features</a>
           </div>
-        </div>
+          {/* Third Navigation Links */}
+          <div className="flex text-lg flex-col items-center md:items-start space-y-3 text-center md:text-left border-b md:border-none pb-4 md:pb-0 md:pb-0 ml-0 md:ml-16">
+          <a href="/privacy-policy" className="hover:underline" style={{ fontFamily: 'LeagueSpartan, sans-serif' }}>Privacy Policy</a>
+          <a href="/" className="hover:underline" style={{ fontFamily: 'LeagueSpartan, sans-serif' }}>Help Center</a>
+          <a href="/#features" className="hover:underline" style={{ fontFamily: 'LeagueSpartan, sans-serif' }}>Features</a>
+          <a href="/#features" className="hover:underline" style={{ fontFamily: 'LeagueSpartan, sans-serif' }}>Features</a>
+          </div>
 
-        {/* Social Media Icons */}
-        <div className="flex justify-center md:justify-between mt-8">
-          <div className="flex space-x-4">
-            {/* WhatsApp */}
-            <a href="https://wa.me/7593970511" target="_blank" rel="noopener noreferrer">
-              <Image src="/whatsapp.png" alt="WhatsApp" width={30} height={30} className="object-contain" />
-            </a>
-            {/* Instagram */}
-            <a href="https://www.instagram.com/the.maadiy?igsh=aTA2OHVzbmJ0end2" target="_blank" rel="noopener noreferrer">
-              <Image src="/instagram24.png" alt="Instagram" width={30} height={30} className="object-contain" />
-            </a>
-            {/* Facebook */}
-            <a href="https://www.facebook.com/profile.php?id=61565378703285" target="_blank" rel="noopener noreferrer">
-              <Image src="/facebook.png" alt="Facebook" width={35} height={35} className="object-contain" />
-            </a>
-            {/* YouTube */}
-            <a href="https://www.youtube.com/channel/UCr9BooZmN_xsauO-lLz_nLw" target="_blank" rel="noopener noreferrer">
-              <Image src="/youtube.png" alt="YouTube" width={35} height={35} className="object-contain" />
-            </a>
-            {/* Email */}
-            <a href="mailto:therahmaneffect@gmail.com" target="_blank" rel="noopener noreferrer">
-              <Image src="/gmail.png" alt="Email" width={35} height={35} className="object-contain" />
-            </a>
-            {/* Phone */}
-            <a href="tel:+919567565069" target="_blank" rel="noopener noreferrer">
-              <Image src="/phone-call.png" alt="Phone" width={35} height={35} className="object-contain" />
-            </a>
-          </div>
         </div>
 
         {/* Copyright Section */}
-        <div className="mt-8 text-center text-sm">
+        <div className="mt-3 md:mt-8 text-center text-sm">
           <p style={{ fontFamily: 'LeagueSpartan, sans-serif' }}>
             &copy; {new Date().getFullYear()} MaaDiy. All rights reserved.
           </p>
